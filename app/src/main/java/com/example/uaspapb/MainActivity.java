ea6f35d83b4bc4f45c6dba6357da07f86fea52b1
hadigone <hadigone123@gmail.com>
2023-06-15 11:23:54 +0700

    tidak ada

--- app/src/main/java/com/example/uaspapb/MainActivity.java
+++ app/src/main/java/com/example/uaspapb/MainActivity.java
@@ -27,6 +27,7 @@ import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
 import java.util.Locale;
+
 public class MainActivity extends AppCompatActivity {
     private String url = "https://mgm.ub.ac.id/todo.php";
     private RecyclerView rctodo;
